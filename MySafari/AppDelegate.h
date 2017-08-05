65b65b8a07f5cc10ad5510833030a43e8eb86f67
TyhOS <tyhos@TyhOSdeMacBook-Air.local>
2017-08-05 10:13:54 +0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by TyhOS on 2017/8/5.
+//  Copyright © 2017年 TyhOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by TyhOS on 2017/8/5.
+//  Copyright © 2017年 TyhOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
